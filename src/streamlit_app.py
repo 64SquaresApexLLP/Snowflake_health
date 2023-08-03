1e193121d54fa792634efa878ae3d116c9941492
Deveshd-64Squares <141302830+Deveshd-64Squares@users.noreply.github.com>
2023-08-03 15:41:38 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import snoflake.connector
 streamlit.header('Snowflake Healthcare App')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
