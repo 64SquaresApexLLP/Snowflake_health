36d21e91973966a4b83d55f54097153bc1dc3159
Deveshd-64Squares <141302830+Deveshd-64Squares@users.noreply.github.com>
2023-08-03 15:26:49 +0530

    Update streamlit_app.py
    TestD

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.header('Snowflake Healthcare App')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+# run a snowflake query and put it all in a var called my_catalog
+my_cur.execute("select * from databases")
+my_catalog = my_cur.fetchall()
+streamlit.dataframe(my_catalog)
