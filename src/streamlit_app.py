0a0e2cdaf603ff1c460e33e1397a82e26eb612af
aishavaidya <141302825+aishavaidya@users.noreply.github.com>
2023-08-04 14:42:56 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,13 +1,4 @@
 import streamlit
 import pandas
-import snowflake.connector
-streamlit.header('Snowflake Healthcare App')
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select * from databases")
-my_catalog = my_cur.fetchall()
-streamlit.dataframe(my_catalog)
-
 st.title("custom layout")
 
