27bdd364ef1a74da2bc40aa8d10aaed46588cda9
husainc-64squares <141302404+husainc-64squares@users.noreply.github.com>
2023-08-04 14:54:25 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.sidebar.button("workloads")
+
+with st.sidebar:
+  selected = option_menu(
+    menu_title = "Main Menu",
+    options = ["Home","Projects","Contact"],
+    icons = ["house","book","envelope"],
+    menu_icon = "cast",
+    default_index = 0,
+    
+  )
+  if selected == "Home":
+    st.title(f"You Have selected {selected}")
+  if selected == "Projects":
+    st.title(f"You Have selected {selected}")
+  if selected == "Contact":
+    st.title(f"You Have selected {selected}")
+
