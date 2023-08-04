2b1a9415ab318353e49d2e5ff408ca2210b5da71
aishavaidya <141302825+aishavaidya@users.noreply.github.com>
2023-08-04 14:50:25 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,6 +1,49 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title('custom layout')
+
+
+def main():
+    # Title of the web app
+    st.title("Custom Layout Example")
+
+    # Create a sidebar
+    st.sidebar.header("Sidebar")
+
+    # Sidebar widgets
+    selected_option = st.sidebar.selectbox("Select an option", ["Option 1", "Option 2", "Option 3"])
+
+    # Main content area
+    st.subheader("Main Content")
+
+    # Conditional content based on the selected option
+    if selected_option == "Option 1":
+        st.write("You selected Option 1.")
+    elif selected_option == "Option 2":
+        st.write("You selected Option 2.")
+    else:
+        st.write("You selected Option 3.")
+
+    # Create a row layout
+    st.subheader("Row Layout")
+
+    col1, col2, col3 = st.columns(3)  # Divide the page into 3 columns
+
+    # Widgets in the first column
+    with col1:
+        st.write("Column 1")
+        st.button("Button 1")
+
+    # Widgets in the second column
+    with col2:
+        st.write("Column 2")
+        st.button("Button 2")
+
+    # Widgets in the third column
+    with col3:
+        st.write("Column 3")
+        st.button("Button 3")
+
 
 
