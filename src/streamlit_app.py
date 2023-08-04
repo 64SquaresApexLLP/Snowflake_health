497a6969f6d68d4a6d45e81fc4d249a24b17a9d1
Deveshd-64Squares <141302830+Deveshd-64Squares@users.noreply.github.com>
2023-08-04 15:44:02 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -9,6 +9,11 @@ my_cur.execute("select * from SWEATSUITS")
 my_catalog = my_cur.fetchall()
 st.dataframe(my_catalog)
 q1 = st.text_input('Write your query','Write the query here')
-my_cur.execute(q1)
-my_catalog = my_cur.fetchall()
-st.dataframe(my_catalog)
+st.button('Run Query')
+if not q1:
+  st.error('Please write a query')
+else:
+  my_cur.execute(q1)
+  my_catalog = my_cur.fetchall()
+  st.dataframe(my_catalog)
+
