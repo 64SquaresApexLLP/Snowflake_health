ba76161c9e9bfec65d49c1e2d88dba1d0b2e9681
Deveshd-64Squares <141302830+Deveshd-64Squares@users.noreply.github.com>
2023-08-04 15:15:50 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 import snowflake.connector
-streamlit.header('Snowflake Healthcare App')
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+st.header('Snowflake Healthcare App')
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select * from SWEATSUITS")
 my_catalog = my_cur.fetchall()
-streamlit.dataframe(my_catalog)
+st.dataframe(my_catalog)
 q1 = st.text_input('Write your query','Write the query here')
 my_cur.execute(q1)
 my_catalog = my_cur.fetchall()
-streamlit.dataframe(my_catalog)
+st.dataframe(my_catalog)
