32dbfeb69c30fa5bcc61e8e597f68d1193b7c089
husainc-64squares <141302404+husainc-64squares@users.noreply.github.com>
2023-08-07 11:36:02 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,11 +4,12 @@ from streamlit_option_menu import option_menu
 
 with st.sidebar:
   selected = option_menu(
-    menu_title = "Main Menu",
+    #menu_title = "Main Menu",
     options = ["Home","Projects","Contact"],
     icons = ["house","book","envelope"],
     menu_icon = "cast",
     default_index = 0,
+    orientation = "Horizontal",
     
   )
   if selected == "Home":
