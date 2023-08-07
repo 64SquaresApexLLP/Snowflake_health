bed6ac2b8ddc16171f721862fccc30c19a97bb47
Deveshd-64Squares <141302830+Deveshd-64Squares@users.noreply.github.com>
2023-08-07 10:46:32 +0530

    Update streamlit_app.py
    Husain's Code Check

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -17,3 +17,38 @@ else:
   my_catalog = my_cur.fetchall()
   st.dataframe(my_catalog)
 
+
+#import streamlit as st
+
+from streamlit_option_menu import option_menu
+
+ 
+
+with st.sidebar:
+
+  selected = option_menu(
+
+    menu_title = "Main Menu",
+
+    options = ["Home","Projects","Contact"],
+
+    icons = ["house","book","envelope"],
+
+    menu_icon = "cast",
+
+    default_index = 0,
+
+
+  )
+
+  if selected == "Home":
+
+    st.title(f"You Have selected {selected}")
+
+  if selected == "Projects":
+
+    st.title(f"You Have selected {selected}")
+
+  if selected == "Contact":
+
+    st.title(f"You Have selected {selected}")
