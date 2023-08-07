978505db471e4d9602394b4eadd9998babe23324
Deveshd-64Squares <141302830+Deveshd-64Squares@users.noreply.github.com>
2023-08-07 11:11:48 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -3,21 +3,7 @@ import pandas as pd
 import snowflake.connector
 import streamlit_option_menu
 from streamlit_option_menu import option_menu
-st.header('Snowflake Healthcare App')
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select * from SWEATSUITS")
-my_catalog = my_cur.fetchall()
-st.dataframe(my_catalog)
-q1 = st.text_input('Write your query','')
-st.button('Run Query')
-if not q1:
-  st.error('Please write a query')
-else:
-  my_cur.execute(q1)
-  my_catalog = my_cur.fetchall()
-  st.dataframe(my_catalog)
+
 
 with st.sidebar:
   selected = option_menu(
@@ -30,6 +16,21 @@ with st.sidebar:
   )
   if selected == "Home":
     st.title(f"You Have selected {selected}")
+    st.header('Snowflake Healthcare App')
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
+    # run a snowflake query and put it all in a var called my_catalog
+    my_cur.execute("select * from SWEATSUITS")
+    my_catalog = my_cur.fetchall()
+    st.dataframe(my_catalog)
+    q1 = st.text_input('Write your query','')
+    st.button('Run Query')
+    if not q1:
+      st.error('Please write a query')
+    else:
+      my_cur.execute(q1)
+      my_catalog = my_cur.fetchall()
+      st.dataframe(my_catalog)
   if selected == "Projects":
     st.title(f"You Have selected {selected}")
   if selected == "Contact":
