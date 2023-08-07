87c0a10f223abfe3c57da7817185040211a46198
aishavaidya <141302825+aishavaidya@users.noreply.github.com>
2023-08-07 12:22:54 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,21 +4,7 @@ import numpy as np
 
 st.sidebar.header("Sidebar")
 
-    # Sidebar widgets
-# selected_option = st.sidebar.selectbox("Select an option", ["Option 1", "Option 2", "Option 3"])
-
-    # Main content area
-# st.subheader("Main Content")
-
-    # Conditional content based on the selected option
-# if selected_option == "Option 1":
-#     st.write("You selected Option 1.")
-# elif selected_option == "Option 2":
-#     st.write("You selected Option 2.")
-# else:
-#     st.write("You selected Option 3.")
-
-    # Create a row layout
+# Create a row layout
 c1, c2= st.columns(2)
 c3, c4= st.columns(2)
 
