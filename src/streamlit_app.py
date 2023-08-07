41df7675bdd5e8b877445ac9c97c3cca74ee7b8c
husainc-64squares <141302404+husainc-64squares@users.noreply.github.com>
2023-08-07 11:43:41 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
-
-  selected = option_menu(
+selected = option_menu(
     menu_title =None,
     options = ["Home","Projects","Contact"],
     icons = ["house","book","envelope"],
     default_index = 0,
     orientation = "horizontal",
   )
-  if selected == "Home":
-    st.title(f"You Have selected {selected}")
-  if selected == "Projects":
-    st.title(f"You Have selected {selected}")
-  if selected == "Contact":
-    st.title(f"You Have selected {selected}")
+if selected == "Home":  
+  st.title(f"You Have selected {selected}")
+if selected == "Projects":
+  st.title(f"You Have selected {selected}")
+if selected == "Contact":
+  st.title(f"You Have selected {selected}")
 
+  
