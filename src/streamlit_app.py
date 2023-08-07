156d46b48b6d4974f6395ebd711d944dfd9b5a1c
Deveshd-64Squares <141302830+Deveshd-64Squares@users.noreply.github.com>
2023-08-07 12:07:13 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -32,6 +32,27 @@ if selected == "Home":
 
 if selected == "Projects":
     st.subheader(f"**You Have selected {selected}**")
+    import streamlit as st
+    import pandas as pd
+    import numpy as np
+    
+    st.sidebar.header("Sidebar")
+    
+        # Sidebar widgets
+    # selected_option = st.sidebar.selectbox("Select an option", ["Option 1", "Option 2", "Option 3"])
+    
+        # Main content area
+    # st.subheader("Main Content")
+    
+        # Conditional content based on the selected option
+    # if selected_option == "Option 1":
+    #     st.write("You selected Option 1.")
+    # elif selected_option == "Option 2":
+    #     st.write("You selected Option 2.")
+    # else:
+    #     st.write("You selected Option 3.")
+    
+        # Create a row layout
     c1, c2= st.columns(2)
     c3, c4= st.columns(2)
     
@@ -44,8 +65,8 @@ if selected == "Projects":
         c4.write("c4")
     
     with c1:
-        df=pd.read_csv("world-data-2023.csv",columns=["Country","Density"])
-        st.write(df)
+        df=pd.read_csv("world-data-2023.csv")
+        st.write(df['Country',density])
         
        
     with c2:
