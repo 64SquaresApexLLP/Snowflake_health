2cc17d373088e355e6f710f02ff4a8af35e80c23
Deveshd-64Squares <141302830+Deveshd-64Squares@users.noreply.github.com>
2023-08-07 11:58:56 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -32,6 +32,34 @@ if selected == "Home":
 
 if selected == "Projects":
     st.subheader(f"**You Have selected {selected}**")
+    c1, c2= st.columns(2)
+    c3, c4= st.columns(2)
+    
+    with st.container():
+        c1.write("c1")
+        c2.write("c2")
+    
+    with st.container():
+        c3.write("c3")
+        c4.write("c4")
+    
+    with c1:
+        df=pd.read_csv("world-data-2023.csv",columns=["Country","Density"])
+        st.write(df)
+        
+       
+    with c2:
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=["a", "b", "c"])
+        st.bar_chart(chart_data)
+    
+    with c3:
+        
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+        st.line_chart(chart_data)
+    
+    with c4:
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+        st.line_chart(chart_data)
 if selected == "Contact":
     st.subheader(f"**You Have selected {selected}**")
 
