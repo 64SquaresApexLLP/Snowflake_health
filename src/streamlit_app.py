f5dc6e785d7b2b6535bf6d5a8f1aff32351f9c53
husainc-64squares <141302404+husainc-64squares@users.noreply.github.com>
2023-08-07 11:40:06 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,12 +4,11 @@ from streamlit_option_menu import option_menu
 
 with st.sidebar:
   selected = option_menu(
-    menu_title = none,
+    menu_title =None,
     options = ["Home","Projects","Contact"],
     icons = ["house","book","envelope"],
     default_index = 0,
     orientation = "horizontal",
-    
   )
   if selected == "Home":
     st.title(f"You Have selected {selected}")
