4fbe8ac7e5553f7fd05ea0a88764cd220e0b24e4
husainc-64squares <141302404+husainc-64squares@users.noreply.github.com>
2023-08-14 16:41:28 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -8,7 +8,7 @@ with st.sidebar:
     selected = option_menu(
     menu_title = "Main Menu",
     options = ["Home","Warehouse","Query Optimization and Processing","Storage","Contact Us"],
-    icons = ["house","gear","activity","😄","envelope"],
+    icons = ["house","gear","activity","snowflake","envelope"],
     menu_icon = "cast",
     default_index = 0,
     #orientation = "horizontal",
