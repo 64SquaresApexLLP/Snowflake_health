98f680d5f7153fd18afc3d6fd8ff0715c124a583
jayeshb64 <141302504+jayeshb64@users.noreply.github.com>
2023-08-14 12:21:33 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,5 +1,8 @@
 import snowflake.connector
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT RETENTION_TIME FROM db_jay.information_schema.tables;")
@@ -7,3 +10,9 @@ my_data_row = my_cur.fetchall()
 st.text("Hello from Snowflake:")
 st.text(my_data_row)
 
+
+df = pd.DataFrame(
+   np.random.randn(10, 5),
+   columns=('col %d' % i for i in range(5)))
+
+st.table(df)
