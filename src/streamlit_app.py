3ee4cd128de295e92bc0aa42e85897d079f76485
husainc-64squares <141302404+husainc-64squares@users.noreply.github.com>
2023-08-14 11:52:42 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -7,7 +7,7 @@ from streamlit_option_menu import option_menu
 with st.sidebar:
     selected = option_menu(
     menu_title = "Main Menu",
-    options = ["Home","Projects","Contact"],
+    options = ["Home","Warehouse","Query Optimization and Processing","Storage","Contact Us"],
     icons = ["house","book","envelope"],
     menu_icon = "cast",
     default_index = 0,
