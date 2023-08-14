08e90c9578ad02b28a647984d0f204a0164bf3b7
jayeshb64 <141302504+jayeshb64@users.noreply.github.com>
2023-08-14 12:12:02 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -7,11 +7,3 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-q1 = st.text_input('Write your query','')
-st.button('Run Query')
-if not q1:
-    st.error('Please write a query')
-else:
-    my_cur.execute(q1)
-    my_catalog = my_cur.fetchall()
-    st.dataframe(my_catalog)
