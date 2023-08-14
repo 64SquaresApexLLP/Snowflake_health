25ee12c9dca69728dce1194104440c4ca31f8ad1
jayeshb64 <141302504+jayeshb64@users.noreply.github.com>
2023-08-14 12:16:09 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,7 +2,7 @@ import snowflake.connector
 import streamlit as st
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT TABLE_SCHEMA,RETENTION_TIME FROM db_jay.information_schema.tables;")
+my_cur.execute("SELECT RETENTION_TIME FROM db_jay.information_schema.tables;")
 my_data_row = my_cur.fetchall()
 st.text("Hello from Snowflake:")
 st.text(my_data_row)
