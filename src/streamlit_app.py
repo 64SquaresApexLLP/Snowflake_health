3ca0c3226c3f0b78e90e2b19c2a1e01638385c82
jayeshb64 <141302504+jayeshb64@users.noreply.github.com>
2023-08-18 11:08:30 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,11 +1,11 @@
-import snowflake.connector
-import streamlit as st
-import pandas as pd
-import numpy as np
+#import snowflake.connector
+#import streamlit as st
+#import pandas as pd
+#import numpy as np
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT TABLE_NAME, ROW_COUNT, RETENTION_TIME FROM db_jay.information_schema.tables;")
-data = my_cur.fetchall()
+3my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT TABLE_NAME, ROW_COUNT, RETENTION_TIME FROM db_jay.information_schema.tables;")
+#data = my_cur.fetchall()
 
 
