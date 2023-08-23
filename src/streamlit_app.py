b2cd9b9a8e0492c6b45e9dae44f8defadbb2ed92
aishavaidya <141302825+aishavaidya@users.noreply.github.com>
2023-08-23 11:28:07 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -16,6 +16,34 @@ with st.sidebar:
 )
 if selected == "Home":
     st.header('Snowflake Healthcare App')
+    # Create a row layout
+    c1, c2= st.columns(2)
+    c3, c4= st.columns(2)
+
+    with st.container():
+        c1.write("c1")
+        c2.write("c2")
+
+    with st.container():
+        c3.write("c3")
+        c4.write("c4")
+
+    with c1:
+        df=pd.read_csv("world-data-2023.csv")
+        st.write(df)
+           
+    with c2:
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=["a", "b", "c"])
+        st.bar_chart(chart_data)
+
+    with c3:
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+        st.line_chart(chart_data)
+
+    with c4:
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+        st.line_chart(chart_data)
+        
     
 if selected == "Warehouse":
     st.subheader(f"**You Have selected {selected}**")
