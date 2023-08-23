9ee016908a44d6d94017276921c1a75d67a975a6
aishavaidya <141302825+aishavaidya@users.noreply.github.com>
2023-08-23 11:40:43 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -15,8 +15,11 @@ with st.container():
     c4.write("c4")
 
 with c1:
-    df=pd.read_csv("world-data-2023.csv")
-    st.write(df)
+    chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
            
 with c2:
     chart_data = pd.DataFrame(np.random.randn(20, 3),columns=["a", "b", "c"])
