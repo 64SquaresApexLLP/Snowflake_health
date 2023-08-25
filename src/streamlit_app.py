894c0f7a39bef738aba7cc045e5717878b8dba9e
Deveshd-64Squares <141302830+Deveshd-64Squares@users.noreply.github.com>
2023-08-25 11:19:12 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -3,12 +3,12 @@ import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
 
-    selected = option_menu(
+selected = option_menu(
     menu_title =None,
     options = ["Home","Projects","Contact"],
     icons = ["house","book","envelope"],
     default_index = 0,
     orientation = "horizontal",
 
-    )
+)
   
