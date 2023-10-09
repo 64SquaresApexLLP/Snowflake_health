196d2cf318afee775b1b838f2382ce4dfe020484
husainc-64squares <141302404+husainc-64squares@users.noreply.github.com>
2023-10-09 20:10:57 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,7 +2,12 @@ import streamlit as st
 import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
+import requests
+import streamlit as st
 
+data = requests.get("'http://3.111.58.87:5000/'").json()
+
+st.write(data)
 selected = option_menu(
     menu_title =None,
     options = ["Home","Projects","Contact"],
